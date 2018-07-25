ab756c2a96bca92b32155ae4831145fcd27e3607
mjmillan <miggy221@gmail.com>
2018-07-25 11:55:07 -0400

    added database

--- app/src/main/java/csi3770/samcalendar/MainActivity.java
+++ app/src/main/java/csi3770/samcalendar/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //private static final String TAG = CustomCalendarActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CalendarCustomView mView = (CalendarCustomView)findViewById(R.id.custom_calendar);
     }
 }
