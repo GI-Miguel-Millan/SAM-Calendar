327e0a64013d0349758d4290cb7ec43dbe5e5c41
mjmillan <miggy221@gmail.com>
2018-08-02 20:34:17 -0400

    grab form data and displayin toast

--- app/src/main/java/csi3770/samcalendar/AddEvent.java
+++ app/src/main/java/csi3770/samcalendar/AddEvent.java
@@ -2,6 +2,9 @@ package csi3770.samcalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,21 +14,46 @@ import java.util.Date;
 
 public class AddEvent extends AppCompatActivity {
     private Date date;
+    private TextView tvDate;
+    private EditText etLocation;
+    private EditText etDetails;
+    private Button btnAddEvent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
+        tvDate = findViewById(R.id.DateSelected); // grab instance of date selected textview
+        etLocation = findViewById(R.id.EditLocation); // grab instance of location editText
+        etDetails = findViewById(R.id.EditDetails); // grab instance of Details editText
+        btnAddEvent = findViewById(R.id.ButtonAddEvent); // grab instance of button
+
+        // Get the date sent from MainActivity
         date = new Date();
         Bundle extras = getIntent().getExtras();
         if (extras !=null) {
             date.setTime(extras.getLong("date"));
         }
 
-        TextView tvDate = findViewById(R.id.DateSelected);
+        // Get the set the date to the event label
         DateFormat df = SimpleDateFormat.getDateInstance();
+        final String date_to_string = df.format(date);
+        tvDate.setText("Event on: " + date_to_string);
 
-        tvDate.setText("Event on: " + df.format(date));
+        btnAddEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String loc = etLocation.getText().toString();
+                String det = etDetails.getText().toString();
+
+                String user_event_data = "date: " + date_to_string +
+                        ", location: " + loc +
+                        ", details: " + det;
+
+                Toast.makeText(AddEvent.this, user_event_data , Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
